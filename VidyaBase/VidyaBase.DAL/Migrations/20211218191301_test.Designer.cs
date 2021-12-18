681e168e8528fcf2ab87fa9afab43bc18f79a853
Stephanie Bracke <r0846614@student.vives.be>
2021-12-18 20:13:53 +0100

    Mock project target framework aangepast naar net5.0, anders kreeg ik migration errors "There is no runtime associated with this framework,...."

--- VidyaBase/VidyaBase.DAL/Migrations/20211218191301_test.Designer.cs
+++ VidyaBase/VidyaBase.DAL/Migrations/20211218191301_test.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VidyaBase.DAL;
 
 namespace VidyaBase.DAL.Migrations
 {
     [DbContext(typeof(VidyaContext))]
-    partial class VidyaContextModelSnapshot : ModelSnapshot
+    [Migration("20211218191301_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VidyaBase/VidyaBase.DAL/Migrations/20211218191301_test.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VidyaBase.DAL.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 75, nullable: false),
+                    EAN = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OwnedGame",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(nullable: false),
+                    GameID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OwnedGame", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publisher",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publisher", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Achievement",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 75, nullable: false),
+                    UserID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Achievement", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Achievement_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Collection",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 75, nullable: false),
+                    UserID1 = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collection", x => x.UserID);
+                    table.ForeignKey(
+                        name: "FK_Collection_Users_UserID1",
+                        column: x => x.UserID1,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wishlist",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 75, nullable: false),
+                    UserID = table.Column<int>(nullable: false),
+                    Note = table.Column<string>(nullable: true, defaultValue: "Your hopes and dreams go here")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlist", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Wishlist_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAchievement",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false),
+                    AchievementID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAchievement", x => new { x.UserID, x.AchievementID });
+                    table.ForeignKey(
+                        name: "FK_UserAchievement_Achievement_AchievementID",
+                        column: x => x.AchievementID,
+                        principalTable: "Achievement",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserAchievement_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CollectionOwnedGame",
+                columns: table => new
+                {
+                    CollectionID = table.Column<int>(nullable: false),
+                    OwnedGamesID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollectionOwnedGame", x => new { x.CollectionID, x.OwnedGamesID });
+                    table.ForeignKey(
+                        name: "FK_CollectionOwnedGame_Collection_CollectionID",
+                        column: x => x.CollectionID,
+                        principalTable: "Collection",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CollectionOwnedGame_Games_OwnedGamesID",
+                        column: x => x.OwnedGamesID,
+                        principalTable: "Games",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WishlistGame",
+                columns: table => new
+                {
+                    WishlistID = table.Column<int>(nullable: false),
+                    GameID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WishlistGame", x => new { x.WishlistID, x.GameID });
+                    table.ForeignKey(
+                        name: "FK_WishlistGame_Games_GameID",
+                        column: x => x.GameID,
+                        principalTable: "Games",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WishlistGame_Wishlist_WishlistID",
+                        column: x => x.WishlistID,
+                        principalTable: "Wishlist",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Achievement_Name",
+                table: "Achievement",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Achievement_UserID",
+                table: "Achievement",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_Name",
+                table: "Category",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collection_UserID1",
+                table: "Collection",
+                column: "UserID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollectionOwnedGame_OwnedGamesID",
+                table: "CollectionOwnedGame",
+                column: "OwnedGamesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_EAN",
+                table: "Games",
+                column: "EAN",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publisher_Name",
+                table: "Publisher",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAchievement_AchievementID",
+                table: "UserAchievement",
+                column: "AchievementID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlist_UserID",
+                table: "Wishlist",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishlistGame_GameID",
+                table: "WishlistGame",
+                column: "GameID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "CollectionOwnedGame");
+
+            migrationBuilder.DropTable(
+                name: "OwnedGame");
+
+            migrationBuilder.DropTable(
+                name: "Publisher");
+
+            migrationBuilder.DropTable(
+                name: "UserAchievement");
+
+            migrationBuilder.DropTable(
+                name: "WishlistGame");
+
+            migrationBuilder.DropTable(
+                name: "Collection");
+
+            migrationBuilder.DropTable(
+                name: "Achievement");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Wishlist");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
